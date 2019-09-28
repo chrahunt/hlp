2d17914b95a37b67ebf08765b49776f9333201fe
Chris Hunt <chrahunt@gmail.com>
2019-09-28 14:30:45 -0400

    Use nox for lint/test/build

---
+++ noxfile.py
@@ -0,0 +1,29 @@
+import nox
+
+
+@nox.session(python=["2.7", "3.7"])
+def test(session):
+    session.install("pytest")
+    session.install(".")
+    session.run("pytest", *session.posargs)
+
+
+@nox.session
+def lint(session):
+    session.install("pre-commit")
+    session.run("pre-commit", "run", *session.posargs)
+
+
+@nox.session
+def build(session):
+    session.install("pep517")
+    session.run(
+        "python",
+        "-m",
+        "pep517.build",
+        "--source",
+        "--binary",
+        "--out-dir",
+        "dist/",
+        ".",
+    )
