1ed8a7a50b5112f4d3958171b93449d4403c3c3d
Chris Hunt <chrahunt@gmail.com>
2019-09-29 17:44:32 -0400

    Update version for release

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with io.open(readme_path, encoding="utf-8") as f:
 
 setup(
     name="hlp",
-    version="0.1.0",
+    version="0.2.0",
     author="Chris Hunt",
     author_email="chrahunt@gmail.com",
     classifiers=[
